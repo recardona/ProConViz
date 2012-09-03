8ec0269865bf662c9f216b97fa2ca0cff6915de3
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2012-09-03 01:36:35 -0400

    Made changes to .html file.
    Was testing on how to use Raphael independently of an html id.  Although WITH the id is recommended, and it does allow to give me a clear outline, will continue without id right now.
    Also, path, wasn't working before, works now.  I think it might have to do either with the CDN i was using.  Changed to one provided by cloudflare.

--- sandbox/raphaeljs/hello-fer/hello1.js
+++ sandbox/raphaeljs/hello-fer/hello1.js
@@ -28,49 +28,14 @@ func2 = function()
     var ellipse = paper.ellipse(200, 400, 100, 50);
 }
 
-func3 = function() 
-{  
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
-    //var drawing = paper.path("M10,20L30,40");
-    //var drawing = paper.path("M 250 250");
-    //drawing.attr("stroke", "#fff");
-    paper.path("M10,10L50,50M50,10L10,50");
-
-}  
-
-func4 =  function() 
-{  
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
-    var tetronimo = paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");  
-  
-    tetronimo.attr({fill: '#9cf', stroke: '#ddd', 'stroke-width': 5});  
-}  
-
-func5 = function()
+func3 = function ()
 {
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
-    var x = 10,
-    y = 20,
-    width = 40,
-    height = 50;
-    // this will draw a rectangular shape equivalent to "M10,20h40v50h-40z"
-    paper.path(Raphael.format("M{0},{1}h{2}v{3}h{4}z", x, y, width, height, -width));
+    var paper = new Raphael(10,50, 640,400);
+    var circle = paper.circle(50,40,10);
+    var c = paper.path("M10 10L90 90");
+    var c = paper.path("M 100 100 L 300 100 L 200 300 z")
+    paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z")
 }
 
-//window.onload = func5
+window.onload = func3
 
-window.onload = function() {  
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
-    var tetronimo = paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");  
-    tetronimo.attr(  
-        {  
-            gradient: '90-#526c7a-#64a0c1',  
-            stroke: '#3b4449',  
-            'stroke-width': 10,  
-            'stroke-linejoin': 'round',  
-            rotation: -90  
-        }  
-    );  
-  
-    tetronimo.animate({rotation: 360}, 2000, 'bounce');  
-} 
